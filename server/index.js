afd9757ac67f3f25b10e30e1de02c24b2916850e
TEI-Sandbox <derekv@trendsinc.com>
2026-03-26 13:18:17 -0700

    fix: use default handler export for React Router v7 on Render

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
-import { createRequestHandler } from "@react-router/node";
-import * as build from "../build/server/index.js";
+import handler from "../build/server/index.js";
 import { createServer } from "node:http";
 
 const port = process.env.PORT || 3000;
 
-const requestHandler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
-
-const server = createServer(requestHandler);
+const server = createServer(handler);
 
 server.listen(port, () => {
   console.log(`🚀 Server listening on port ${port}`);
